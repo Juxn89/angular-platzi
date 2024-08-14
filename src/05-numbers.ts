f2a2a43662ab8a81874f00408ddd471b465eebfc
Juan Gómez <gomezj01@stepstone.com>
2024-08-14 14:38:19 -0600

    Numbers

---
+++ src/05-numbers.ts
@@ -0,0 +1,13 @@
+( () => {
+  let productPrice = 100
+  productPrice = 12
+
+  console.log('productPrice', productPrice)
+
+  let customerAge: number = 28
+  customerAge = customerAge + 1
+  // customerAge = customerAge + '1'
+
+  let hex = 0xfff
+  let bin = 0b1010
+} )()
