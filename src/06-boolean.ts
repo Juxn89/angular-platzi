c03b022951d409199f1e9cc50492fc857e019a96
Juan Gómez <gomezj01@stepstone.com>
2024-08-14 14:43:48 -0600

    Boolean

---
+++ src/06-boolean.ts
@@ -0,0 +1,13 @@
+( () => {
+  let isEnable = true
+  isEnable = false
+
+  let isNew:boolean = false
+  console.log('isNew', isNew)
+  isNew = true
+  console.log('isNew', isNew)
+
+  const random = Math.random()
+  console.log('random', random)
+  isNew = random >= 0.5
+} )()
