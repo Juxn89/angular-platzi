5145f89941141f3524948ec66758edbdbd77ac86
Juan Gómez <gomezj01@stepstone.com>
2024-08-14 14:48:59 -0600

    Strings

---
+++ src/07-strings.ts
@@ -0,0 +1,18 @@
+( () => {
+  let productTitle = 'My amazing product'
+  // productTitle = null
+  // productTitle = () => {}
+  // productTitle = 123
+  productTitle= 'Title updated'
+  console.log('productTitle', productTitle)
+
+  const producDescription = "I'm bla bla bla..."
+  console.log('productDescription', producDescription)
+
+  const summary = `
+    I'm a description :)
+    title: ${ productTitle }
+  `;
+
+  const myString: string = ''
+} )()
