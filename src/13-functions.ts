cbf3eedc4234ade382303b2558724c1b66938fd0
Juan Gómez <gomezj01@stepstone.com>
2024-08-15 13:38:42 -0600

    Functions

---
+++ src/13-functions.ts
@@ -0,0 +1,26 @@
+(() => {
+  type Size = 'S' | 'M' | 'L' | 'XL'
+
+  function createProductToJson (title: string, createdAt: Date, stock: number, size: Size) {
+    return {
+      title,
+      createdAt,
+      stock,
+      size
+    }
+  }
+
+  const createProductToJsonV2 = (title: string, createdAt: Date, stock: number, size?: Size) => {
+    return {
+      title,
+      createdAt,
+      stock,
+      size
+    }
+  }
+
+  const product1 = createProductToJson('Product #1', new Date(), 1, 'M')
+  console.log(product1)
+
+  const product2 = createProductToJsonV2('Product #1', new Date(), 1)
+})()
