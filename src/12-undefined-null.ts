f2fbb2c80fdee986ba450ecbdeef6d600438e3c0
Juan Gómez <gomezj01@stepstone.com>
2024-08-15 12:02:56 -0600

    Null and undefined

---
+++ src/12-undefined-null.ts
@@ -0,0 +1,9 @@
+( () => {
+  // let myNumber: number = undefined
+  // let myString: string = null
+
+  let myNull: null = null
+  let myUndefined: undefined = undefined
+
+  let myNumber: number | null;
+} )()
