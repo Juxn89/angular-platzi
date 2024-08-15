5495db4cb548a6b58afe45d57ed84839f3b20c27
Juan Gómez <gomezj01@stepstone.com>
2024-08-15 10:47:05 -0600

    Arrays

---
+++ src/08-arrays.ts
@@ -0,0 +1,18 @@
+( () => {
+  let prices = [1, 2, 2, 1, 1, 212]
+
+  // prices.push('asas')
+  // prices.push(true)
+  // prices.push({})
+
+  prices.push(12312)
+  prices = [1, 2, 3, 4, 5]
+
+  let prices2 = ['hi', true]
+  // prices2.push(123)
+
+  let prices3: (number | string | boolean)[] = ['1111', true]
+  prices3.push(21312)
+  // prices.push({})
+  // prices.push([])
+} )()
