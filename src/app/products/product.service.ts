78373987b93f503e6107492dfca97a4dfe42cb6f
Juan Gómez <gomezj01@stepstone.com>
2024-08-20 14:43:06 -0600

    Omit and pick type

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,5 +1,19 @@
 import { Product } from './product.model'
+import { CreateProductDto } from './product.dto'
 
 export const products: Product[] = []
 
-export const addProduct = (data: Product) => products.push(data)
+export const addProduct = (data: CreateProductDto) => {
+  products.push({
+    ...data,
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    category: {
+      id: crypto.randomUUID(),
+      name: 'Warrior',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  })
+}
