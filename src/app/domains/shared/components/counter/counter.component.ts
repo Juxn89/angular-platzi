08763512d8f388fd49a1ae6543603eea0500dddc
Juan Gómez <gb.jc@outlook.com>
2024-09-13 13:22:01 -0400

    Angular lifecycle: ngOnInit, ngAfterViewInit, ngOnDestroy.

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -11,17 +11,42 @@ export class CounterComponent {
   @Input({ required: true }) duration: number = 0
   @Input({ required: true }) message: string = ''
 
+  private readonly numberOfRepets: number = 30
+
   constructor() {
     // No async
     // Before render
+    // Run Once
     console.log('1. Constructor')
-    console.log('-'.repeat(10))
+    console.log('-'.repeat(this.numberOfRepets))
   }
 
   ngOnChanges(changes: SimpleChange) {
     // Before and during render
     console.log('2. ngOnChange -> before and during render')
     console.log(changes)
-    console.log('-'.repeat(10))
+    console.log('-'.repeat(this.numberOfRepets))
+  }
+
+  ngOnInit() {
+    // Afeter render
+    // Run once
+    // Async, then, subs
+    console.log('3. ngOnInit')
+    console.log('duration -> ', this.duration)
+    console.log('message -> ', this.message)
+    console.log('-'.repeat(this.numberOfRepets))
+  }
+
+  ngAfterViewInit() {
+    // After render
+    // Childrens are already printed
+    console.log('4. ngAfterViewInit')
+    console.log('-'.repeat(this.numberOfRepets))
+  }
+
+  ngOnDestroy() {
+    console.log('5. ngOnDestroy')
+    console.log('-'.repeat(this.numberOfRepets))
   }
 }
