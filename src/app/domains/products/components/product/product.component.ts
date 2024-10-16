c639f3f1c0d171042c9717c7f648e44188d7af6d
Juan Gómez <gb.jc@outlook.com>
2024-10-16 16:49:34 -0400

    Use pipes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Product } from '../../../shared/components/counter/models/products.model';
+
+import { Product } from '@shared/components/counter/models/products.model';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
