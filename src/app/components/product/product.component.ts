26acfae5f96e1712ae2771f5b99ad1afaa5d8a29
Juan Gómez <gb.jc@outlook.com>
2024-10-28 14:02:51 -0400

    Product component

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Pipe } from '@angular/core';
+import { Product } from '@models/product.model';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss'
+})
+export class ProductComponent {
+  @Input() product!: Product;
+}
